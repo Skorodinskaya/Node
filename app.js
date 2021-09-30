f1880326892c8edce9ad5171586b0ada1d47044d
Skorodinskaya <skorodinskaya01@gmail.com>
2021-09-30 11:00:22 +0300

    changes

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const builder = require('./helper/user-builder');
+// const builder = require('./helper/user-builder');
+//
+// let user = builder.createUser('Anna',20);
+//
+// console.log(user);
 
-let user = builder.createUser('Anna',20);
+const fs = require('fs');
 
-console.log(user);
+fs.readFile(`${__dirname}/helper/user-builder.js`, ((err, data )=> {
+    if(err) {
+        console.log(err)
+        return;
+    }
+    console.log(data.toString())
+}));
