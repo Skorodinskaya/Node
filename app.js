f42f31fbd285ea58ad1dbe8fcade5529b4b873ff
Skorodinskaya <skorodinskaya01@gmail.com>
2021-10-02 15:41:37 +0300

    changes

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-// const builder = require('./helper/user-builder');
-//
-// let user = builder.createUser('Anna',20);
-//
-// console.log(user);
-
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile(`${__dirname}/helper/user-builder.js`, ((err, data )=> {
-    if(err) {
-        console.log(err)
-        return;
-    }
-    console.log(data.toString())
-}));
+const mkdirBoys = path.join(__dirname, 'files', 'boys')
+const mkdirGirls = path.join(__dirname, 'files', 'girls')
+
+fs.mkdir(mkdirBoys, {recursive: true}, (e) => {
+    console.log(e)
+});
+
+fs.mkdir(mkdirGirls, {recursive: true}, (e) => {
+    console.log(e)
+});
+
+const correctFolder = (currentState, gender, move) => {
+    fs.readdir(currentState, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        data.forEach(value => {
+            fs.readFile(path.join(currentState, value), (err, data) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+
+                const item = JSON.parse(data);
+                if (item.gender === gender) {
+                    fs.rename(path.join(currentState, value), path.join(move, value), (err) => {
+                        console.log(err);
+                    })
+                }
+            })
+        })
+    })
+}
+
+correctFolder(mkdirGirls, 'male', mkdirBoys);
+correctFolder(mkdirBoys, 'female', mkdirGirls);
